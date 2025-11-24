84ad6ba567f1fe9c9165254e7dda4d8e05cc28ff
FabioHenriqueSCC <fhscc7@gmail.com>
2025-11-24 11:30:38 -0300

    feat: create project and abstract class person

---
+++ src/models/Person/Person.java
@@ -0,0 +1,11 @@
+package models.Person;
+
+public abstract class Person {
+    protected String name;
+    protected String email;
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
